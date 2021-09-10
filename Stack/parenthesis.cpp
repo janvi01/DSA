eb7848fbdf4c8041cdb4b3f869aa17ba139d5a5e
Janvi Bajoria <janvibajo1@gmail.com>
2021-09-10 15:28:14 +0530

    updated

---
+++ Stack/parenthesis.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+bool check(string str, int n)
+{
+    stack<char> s;
+    for (int i = 0; i < n; i++)
+    {
+        if (str[i] == '(' || str[i] == '{' || str[i] == '[')
+            s.push(str[i]);
+        else
+        {
+
+            if (s.empty())
+                return false;
+
+            if (s.top() == '(')
+            {
+                if (str[i] == '}' || str[i] == ']')
+                    return false;
+                else
+                    return true;
+            }
+            if (s.top() == '[')
+            {
+                if (str[i] == '}' || str[i] == ')')
+                    return false;
+                else
+                    return true;
+            }
+            if (s.top() == '{')
+            {
+                if (str[i] == ']' || str[i] == ')')
+                    return false;
+                else
+                    return true;
+            }
+        }
+    }
+}
+
+int main()
+{
+    string str;
+    cout << "Enter string to check balance parenthesis = ";
+    cin >> str;
+    if (check(str, str.length()))
+        cout << "Balanced\n";
+    else
+        cout << "Unbalanced\n";
+    return 0;
+}
